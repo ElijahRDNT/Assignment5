336cc50d99bfb43e413e5abdda03ddd98bde35bb
LAPTOP-7O8QBV3P\lenovo <elijahmaetardiente@iskolarngbayan.pup.edu.ph>
2021-11-16 22:19:03 +0800

    added a function for age validation

--- program3_life_stages.py
+++ program3_life_stages.py
@@ -14,5 +14,15 @@ def get_input():
     age_input = input("Enter your age to classify your life stage: ")
     return age_input
 
+
+def age_validation(age_try):
+    try:
+        age_int = int(age_try)
+        return age_int
+    except ValueError:
+        print("\nInvalid age. Please enter whole numbers.")
+        return False    
+
 display_header()
 age = get_input()
+valid_age = age_validation(age)
