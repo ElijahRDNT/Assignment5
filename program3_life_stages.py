60c491729623ba453ac9d4555d74c500e5f1561a
LAPTOP-7O8QBV3P\lenovo <elijahmaetardiente@iskolarngbayan.pup.edu.ph>
2021-11-16 22:36:07 +0800

    added a function for the classification of life stages

--- program3_life_stages.py
+++ program3_life_stages.py
@@ -20,9 +20,26 @@ def age_validation(age_try):
         age_int = int(age_try)
         return age_int
     except ValueError:
-        print("\nInvalid age. Please enter whole numbers.")
+        print("\nERROR: Invalid age. Please enter whole numbers.\n")
         return False    
+
+
+def life_stage_classification(age_stage):
+    if (0 <= age_stage <= 12):
+        print ("\nLife stage: Kid\n")
+    elif (13 <= age_stage <= 17):
+        print ("\nLife stage: Teen\n")
+    elif (age_stage == 18):
+        print ("\nLife stage: Debut\n")
+    elif (19 <= age_stage <= 150):
+        print ("\nLife stage: Adult\n")
+    elif (age_stage >= 151):
+        print("\nSorry. You exceeded the age limit. Cannot be classified.\n")
+    elif (age_stage <= -1):
+        print("\nInvalid age. Less than 0 inputs are not accepted.\n")
 
 display_header()
 age = get_input()
 valid_age = age_validation(age)
+if valid_age != False:
+    life_stage_classification(valid_age)
