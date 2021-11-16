64dadf6e800840bb6797f9d30266aa4cbe6def66
LAPTOP-7O8QBV3P\lenovo <elijahmaetardiente@iskolarngbayan.pup.edu.ph>
2021-11-17 00:33:21 +0800

    I added a function for input validation that restricts inputs that are not convertable into float datatype. I also added functions to classify the equivalent grade mark and description of the grade input.

--- program1_PUP_Grading_System.py
+++ program1_PUP_Grading_System.py
@@ -12,4 +12,99 @@ def get_input():
         "\nNote: Avoid putting unnecessary symbols. Only numbers and a decimal point(if applicable) are allowed. Thank you.\n\nInput grade: ")
     return grade_input
 
+
+def input_validation(grade_try):
+    try:
+        grade_float = float(grade_try)
+        return grade_float
+
+    except ValueError:
+        print(
+            "\nInvalid input. Please enter numbers and a decimal point(if applicable) only.\n")
+        return False
+
+
+def grade_classification(grade_classify):
+    if (65 <= grade_classify <= 74):
+        mark = 5.0
+        print(f"Grade/Mark: {mark}")
+        return mark
+
+    elif (grade_classify == 75):
+        mark = 3.0
+        print(f"Grade/Mark: {mark}")
+        return mark
+
+    elif(76 <= grade_classify <= 78):
+        mark = 2.75
+        print(f"Grade/Mark: {mark}")
+        return mark
+
+    elif(79 <= grade_classify <= 81):
+        mark = 2.5
+        print(f"Grade/Mark: {mark}")
+        return mark
+
+    elif(82 <= grade_classify <= 84):
+        mark = 2.25
+        print(f"Grade/Mark: {mark}")
+        return mark
+
+    elif(85 <= grade_classify <= 87):
+        mark = 2.0
+        print(f"Grade/Mark: {mark}")
+        return mark
+
+    elif(88 <= grade_classify <= 90):
+        mark = 1.75
+        print(f"Grade/Mark: {mark}")
+        return mark
+
+    elif(91 <= grade_classify <= 93):
+        mark = 1.5
+        print(f"Grade/Mark: {mark}")
+        return mark
+
+    elif(94 <= grade_classify <= 96):
+        mark = 1.25
+        print(f"Grade/Mark: {mark}")
+        return mark
+
+    elif(97 <= grade_classify <= 100):
+        mark = 1.0
+        print(f"Grade/Mark: {mark}")
+        return mark
+
+
+def grade_description(grade_describe):
+    if (grade_describe == 1.0) or (grade_describe == 1.25):
+        description = "Excellent"
+        print(f"Description: {description}\n")
+
+    elif (grade_describe == 1.5) or (grade_describe == 1.75):
+        description = "Very Good"
+        print(f"Description: {description}\n")
+
+    elif (grade_describe == 2.0) or (grade_describe == 2.25):
+        description = "Good"
+        print(f"Description: {description}\n")
+
+    elif (grade_describe == 2.5) or (grade_describe == 2.75):
+        description = "Satisfactory"
+        print(f"Description: {description}\n")
+
+    elif (grade_describe == 3.0):
+        description = "Passing"
+        print(f"Description: {description}\n")
+
+    elif (grade_describe == 5.0):
+        description = "Failure"
+        print(f"Description: {description}\n")
+
+
 grade = get_input()
+valid_grade = input_validation(grade)
+
+if (valid_grade != False):
+    grade_classified = grade_classification(valid_grade)
+    grade_described = grade_description(grade_classified)
